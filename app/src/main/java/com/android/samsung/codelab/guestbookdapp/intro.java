1b01578734ee430f031d753d0741d94ab1c1f30c
서건식 <iqeq2328@hanyang.ac.kr>
2020-02-12 19:25:49 +0900

    Add ToDo

--- app/src/main/java/com/android/samsung/codelab/guestbookdapp/intro.java
+++ app/src/main/java/com/android/samsung/codelab/guestbookdapp/intro.java
@@ -1,16 +1,7 @@
 package com.android.samsung.codelab.guestbookdapp;
 
-import android.content.Intent;
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-
-import com.android.samsung.codelab.guestbookdapp.presenter.IntroPresenter;
 
 import info.hoang8f.widget.FButton;
 

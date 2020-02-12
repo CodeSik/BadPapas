03884f19d8ecec5d8c4a9075547b188d8cefc912
hyunjiHong <onlygod0807@gmail.com>
2020-02-12 22:40:52 +0900

    test1

--- app/src/main/java/com/android/samsung/codelab/guestbookdapp/copy_key.java
+++ app/src/main/java/com/android/samsung/codelab/guestbookdapp/copy_key.java
@@ -1,4 +1,36 @@
 package com.android.samsung.codelab.guestbookdapp;
 
-public class copy_key {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import info.hoang8f.widget.FButton;
+
+public class copy_key extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.copy_key);
+
+        //버튼 객체 생성
+       // FButton copyKey = (FButton) findViewById(R.id.copy);
+
+        //버튼 효과
+        /*copyKey.setButtonColor(getResources().getColor(R.color.fbutton_color_concrete));
+        copyKey.setShadowColor(getResources().getColor(R.color.fbutton_color_asbestos));
+        copyKey.setShadowEnabled(true);
+        copyKey.setShadowHeight(5);
+        copyKey.setCornerRadius(5);
+*/
+       /* button1.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(intro.this, );
+                startActivity(intent);
+            }
+        });*/
+
+
+    }
+
+
 }

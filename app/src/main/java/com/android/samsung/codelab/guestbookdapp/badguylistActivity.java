af2e113a86bd3aa86e97cb4fa12b1aa696b8c310
hyunjiHong <37855284+hyunjiHong@users.noreply.github.com>
2020-02-13 17:36:16 +0900

    Merge branch 'master' of D:\BadPapas with conflicts.

--- app/src/main/java/com/android/samsung/codelab/guestbookdapp/badguylistActivity.java
+++ app/src/main/java/com/android/samsung/codelab/guestbookdapp/badguylistActivity.java
@@ -1,26 +1,40 @@
 package com.android.samsung.codelab.guestbookdapp;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
-public class badguylistActivity {
-  /*  @Override
+public class badguylistActivity extends AppCompatActivity {
+
+    private ListViewAdapter adapter;
+    private ListView listView;
+
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.badguy_list);
+       ListView listview ;
+       ListViewAdapter adapter;
 
-        iv = (ImageView)findViewById(R.id.imageView1);
+       // Adapter 생성
+       adapter = new ListViewAdapter() ;
 
-        // res/drawable 폴더에 있는 이미지로 셋팅하기
-        iv.setImageResource(R.drawable.a1);
+       // 리스트뷰 참조 및 Adapter달기
+       listview = (ListView) findViewById(R.id.listView);
+       listview.setAdapter(adapter);
 
-        iv.setOnClickListener(new MyListener());
-
-    } // end onCreate
-
-    class MyListener implements View.OnClickListener {
+       // 첫 번째 아이템 추가.
+       adapter.addItem(ContextCompat.getDrawable(this, R.drawable.a1),
+               "Box", "Account Box Black 36dp") ;
+       // 두 번째 아이템 추가.
+       adapter.addItem(ContextCompat.getDrawable(this, R.drawable.a2),
+               "Circle", "Account Circle Black 36dp") ;
+       // 세 번째 아이템 추가.
+       adapter.addItem(ContextCompat.getDrawable(this, R.drawable.a3),
+               "Ind", "Assignment Ind Black 36dp") ;
+}
+  /*  class MyListener implements View.OnClickListener {
 
         int i = 0;
         int length = ImageId.length;

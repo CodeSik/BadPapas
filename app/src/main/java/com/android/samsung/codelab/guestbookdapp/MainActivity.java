c2cfc44800ceff2cf9355106bd6336f204830f4f
ttc1018 <emforhs246@naver.com>
2020-02-13 13:28:02 +0900

    13:27 (splash->inform)

--- app/src/main/java/com/android/samsung/codelab/guestbookdapp/MainActivity.java
+++ app/src/main/java/com/android/samsung/codelab/guestbookdapp/MainActivity.java
@@ -16,18 +16,6 @@ public class MainActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final Handler handler = new Handler();
-        handler.postDelayed((new Runnable() {
-            @Override
-            public void run(){
-                Intent intent = new Intent(getApplicationContext(), intro.class);
-                startActivity(intent);
-                finish();
-            }
-
-        }), 4000);
-
     }
 
 
--- app/src/main/java/com/android/samsung/codelab/guestbookdapp/SplashActivity.java
+++ app/src/main/java/com/android/samsung/codelab/guestbookdapp/SplashActivity.java
@@ -1,17 +1,15 @@
 package com.android.samsung.codelab.guestbookdapp;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import com.android.samsung.codelab.guestbookdapp.MainActivity;
-import com.android.samsung.codelab.guestbookdapp.R;
-import com.kenai.jffi.Main;
+import io.reactivex.annotations.Nullable;
 
-public class SplashActivity extends Activity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         try{
             Thread.sleep(4000);
@@ -19,7 +17,6 @@ public class SplashActivity extends Activity {
             e.printStackTrace();
         }
         Intent intent = new Intent(this, Information.class);
-        intent.putExtra("state", "launch");
         startActivity(intent);
         finish();
         }
